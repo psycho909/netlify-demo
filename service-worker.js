6361088a8a813341d0b80452fe5cfe875dcd7581
psycho909 <psychosocial909@gmail.com>
2021-09-23 23:37:26 +0800

    23123

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,28 @@
-// self.addEventListener("install", function (event) {
-// 	self.skipWaiting();
+self.addEventListener("install", (event) => {
+	console.log("V1 installing…");
+});
+self.addEventListener("activate", (event) => {
+	console.log("V1 now ready to handle fetches!");
+});
 
-// 	var offlinePage = new Request("offline.html");
-// 	event.waitUntil(
-// 		fetch(offlinePage).then(function (response) {
-// 			return caches.open("offline2").then(function (cache) {
-// 				return cache.put(offlinePage, response);
-// 			});
-// 		})
-// 	);
-// });
-// self.addEventListener("fetch", function (event) {
-// 	event.respondWith(
-// 		fetch(event.request).catch(function (error) {
-// 			return caches.open("offline2").then(function (cache) {
-// 				return cache.match("offline.html");
-// 			});
-// 		})
-// 	);
-// });
-// self.addEventListener("refreshOffline", function (response) {
-// 	return caches.open("offline2").then(function (cache) {
-// 		return cache.put(offlinePage, response);
-// 	});
-// });
-// self.addEventListener("push", function (event) {
-// 	var data = event.data.json();
-// 	var opts = {
-// 		body: data.body,
-// 		icon: data.icon,
-// 		data: {
-// 			url: data.url,
-// 		},
-// 	};
-// 	event.waitUntil(self.registration.showNotification(data.title, opts));
-// });
-// self.addEventListener("notificationclick", function (event) {
-// 	var data = event.notification.data;
-// 	event.notification.close();
-// 	event.waitUntil(clients.openWindow(data.url));
-// });
+self.addEventListener("fetch", (event) => {
+	console.log("now fetch!");
+	console.log(event.request);
+	event.respondWith(
+		//當有重複的請求時，就直接拿cache裡的response當回應，沒有的話就送出請求，並且把回應存到cache裡
+		caches.match(event.request).then(function (response) {
+			return (
+				response ||
+				fetch(event.request).then((res) =>
+					// 存 caches 之前，要先打開 caches.open(dataCacheName)
+					caches.open(dataCacheName).then(function (cache) {
+						// cache.put(key, value)
+						// 下一次 caches.match 會對應到 event.request
+						cache.put(event.request, res.clone());
+						return res;
+					})
+				)
+			);
+		})
+	);
+});

7324981156005e5f8609ddc28fb170e9c9800506
Ching CHEN <chingchen@gamania.com>
2022-12-27 15:01:29 +0800

    wwqeqeqwe

---
+++ event/js/lightbox.js
@@ -0,0 +1,36 @@
+function Gallery(className, movie, scroll = true) {
+	var defaultObj = {
+		addClass: "default",
+		hasCloseBtn: true,
+		hasActionBtn: false,
+		clickBgClose: true,
+		afterOpen: function () {
+			if (scroll == true) {
+				$(".lb-pop").mCustomScrollbar({
+					theme: "light",
+					contentTouchScroll: true,
+					mouseWheel: {
+						preventDefault: true,
+					},
+					advanced: { extraDraggableSelectors: ".lb-pop" },
+				});
+			}
+		},
+		afterClose: function () {
+			$.gbox.close();
+		},
+	};
+	defaultObj.addClass = `default ${className}`;
+	if (movie) {
+		movie = `./assets/css/video/${movie}.mp4`;
+	}
+	var HTML = `<div class="lb-content lb-pop"><div class="lb-info">${movie ? '<video src="' + movie + '" muted autoplay playsinline loop></video>' : ""}</div></div>`;
+	$.gbox.open(HTML, defaultObj);
+}
+
+$(".lb-btn").on("click", function () {
+	let pop = $(this).attr("data-pop");
+	let movie = $(this).attr("data-movie");
+	let scroll = $(this).attr("data-scroll");
+	Gallery(pop, movie, scroll);
+});

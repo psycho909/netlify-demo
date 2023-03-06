ae51d13e2ace7e8749ef5e7a61e615d03b9b91e3
Ching CHEN <chingchen@gamania.com>
2023-03-06 10:35:54 +0800

    asdasdsadsad

--- pwa/sw.js
+++ pwa/sw.js
@@ -18,17 +18,46 @@ self.addEventListener("fetch", (event) => {
 		event.respondWith(caches.match("./images/dog.cat"));
 	}
 });
-self.addEventListener("notificationclick", function (event) {
-	var notification = event.notification;
-	var action = event.action;
-
-	console.log(notification);
-
-	if (action === "confirm") {
-		console.log("Confirm was chosen");
-		notification.close();
-	} else {
-		console.log(action);
-		notification.close();
+self.addEventListener("notificationclick", (event) => {
+	const notification = event.notification;
+	const action = event.action;
+	const link = notification.data.link;
+	const link_ok = notification.data.link_ok;
+	const link_ng = notification.data.link_ng;
+	switch (action) {
+		case "yes":
+			if (link_ok) {
+				clients.openWindow(link_ok);
+			}
+			break;
+		case "no":
+			if (link_ng) {
+				clients.openWindow(link_ng);
+			}
+			break;
+		case "close":
+			break;
+		default:
+			if (link) {
+				clients.openWindow(link);
+			}
+			break;
 	}
+	notification.close();
+	console.log("notificationclick action is", action);
+});
+
+self.addEventListener("push", (event) => {
+	console.log("[Service Worker] Push Received.");
+	let title = "Server Push";
+	let options = {
+		body: "push TEST",
+		icon: "./assets/images/android_048.png"
+	};
+	if (event.data) {
+		options = event.data.json();
+		title = options.title;
+	}
+
+	event.waitUntil(self.registration.showNotification(title, options));
 });

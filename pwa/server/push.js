9e289e5ec2f8bc14ea45b765014f4858898e3e41
Ching CHEN <chingchen@gamania.com>
2023-03-06 14:49:22 +0800

    asdasdasdasd

--- pwa/server/push.js
+++ pwa/server/push.js
@@ -16,12 +16,13 @@ webpush.setVapidDetails("mailto:psychosocial909@gmail.com", vapidKeys.publicKey,
 
 const options = {
 	icon: "assets/images/android_048.png",
-	body: "Angular 測試工作坊 9月23日(六)",
+	title: "測試推播功能V1",
+	body: "測試推播功能",
 	image: "https://scontent.ftpe7-1.fna.fbcdn.net/v/t31.0-8/21273134_10156585628499554_8520027102111869914_o.jpg?oh=9d7bcbc999c161f5ce778e361a4b9ea4&oe=5A47D9EE",
 	data: {
-		link: "https://www.facebook.com/groups/augularjs.tw/",
-		link_ok: "https://www.facebook.com/events/188912961650574/?acontext=%7B%22ref%22%3A%224%22%2C%22feed_story_type%22%3A%22370%22%2C%22action_history%22%3A%22null%22%7D",
-		link_ng: "https://www.facebook.com/groups/angularstudygroup/"
+		link: "https://tw.beanfun.com/",
+		link_ok: "https://tw.beanfun.com/",
+		link_ng: "https://tw.beanfun.com/"
 	},
 	requireInteraction: true,
 	actions: [

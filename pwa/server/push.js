9824febe3d66bad0d90a4974240874445e56a536
Ching CHEN <chingchen@gamania.com>
2023-03-08 10:43:56 +0800

    wqeqweqweqwe

--- pwa/server/push.js
+++ pwa/server/push.js
@@ -7,25 +7,22 @@ const vapidKeys = {
 
 // const vapidKeys = webpush.generateVAPIDKeys();
 // console.log(vapidKeys);
-const browserKeys = {
-	p256dh: "BOKcagDgrewVjbh2scGX_6zfzqXfrXPYgaUDoRwFkU-lH6nXrK3QkHUyaRmmx5gjNW0QzV0jofQVtBCLffRj8SY",
-	auth: "q9EeAk_i7fN80rJ_HJwSFQ"
-};
 
 webpush.setVapidDetails("mailto:psychosocial909@gmail.com", vapidKeys.publicKey, vapidKeys.privateKey);
 
 const options = {
-	icon: "assets/images/android_048.png",
-	title: "測試推播功能V1",
-	body: "測試推播功能",
-	image: "https://scontent.ftpe7-1.fna.fbcdn.net/v/t31.0-8/21273134_10156585628499554_8520027102111869914_o.jpg?oh=9d7bcbc999c161f5ce778e361a4b9ea4&oe=5A47D9EE",
+	icon: "https://tw.hicdn.beanfun.com/beanfun/promo/MapleStory/E20230104/assets/maple-story-logo@3x-8fe07918.png", // 網站icon圖示
+	title: "測試推播功能V1", // 標題
+	body: "測試推播功能", // 內文
+	image: "https://tw.hicdn.beanfun.com/beanfun/WebImage/1606431240888.png", // 推播大圖
 	data: {
-		link: "https://tw.beanfun.com/",
-		link_ok: "https://tw.beanfun.com/",
-		link_ng: "https://tw.beanfun.com/"
+		link: "https://world-flipper.beanfun.com/", // 直接點通知的連結
+		link_ok: "https://tw.hicdn.beanfun.com/beanfun/promo/MapleStory/E20230104/index.html", // 參加
+		link_ng: "https://tw.beanfun.com/" // 不參加
 	},
-	requireInteraction: true,
+	requireInteraction: true, // 直到用戶點擊或關閉才會關閉
 	actions: [
+		// 按鈕
 		{
 			action: "yes",
 			title: "參加",
@@ -43,8 +40,8 @@ const subscription = {
 	endpoint: "https://wns2-ln2p.notify.windows.com/w/?token=BQYAAABdilSpHDyQCFKlZAhjfO7fyyORvGUKT7%2fA8MK%2bPHgqWIT%2bjFHWU5g4PKU8kXOhnz6vA3n8tupfhCK1TxBljrdwLDWeLXa9AlCu1yRGt3qMXpDpcl3vMfJ1cNNq22QWdd8gvqxmxdOtjpdZInsWNYjX%2fV%2bNXH6ziLf%2bJqUEEYnxyJkiunV%2be0bEm07UyV3GZ5pmNi8vSBVWKYv9dCOzWLF2AuiAe%2bjqmTjgIntfA%2fJtxq4ffkpoV0AkykRGDahLNdzmtwMH%2fp3czXuM2aFNWNwTmWlZlH0qBqNrRTq%2fsH1xpFRm3BjSzxUDz%2f5vzbVM8GV6rw%2fQhJN4sdqywdvXk8jK",
 	expirationTime: null,
 	keys: {
-		p256dh: browserKeys.p256dh,
-		auth: browserKeys.auth
+		p256dh: "BOKcagDgrewVjbh2scGX_6zfzqXfrXPYgaUDoRwFkU-lH6nXrK3QkHUyaRmmx5gjNW0QzV0jofQVtBCLffRj8SY",
+		auth: "q9EeAk_i7fN80rJ_HJwSFQ"
 	}
 };
 

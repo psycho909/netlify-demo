4e19d78f0909d71081a9999651c4097d163434e6
Ching CHEN <chingchen@gamania.com>
2023-04-26 10:53:11 +0800

    asdasd

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -33,7 +33,7 @@ messaging.onBackgroundMessage(function (payload) {
 	// };
 
 	if (payload.data) {
-		options = payload.data.body.json();
+		options = payload.data.options.json();
 		title = payload.data.title;
 	}
 	self.registration.showNotification(title, options);

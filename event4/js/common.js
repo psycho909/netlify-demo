f7b9cf7603b1fa81a4b3fdd5518127cd862b3351
psycho909 <psychosocial909@gmail.com>
2024-12-19 23:11:52 +0800

    asdasdas

--- event4/js/common.js
+++ event4/js/common.js
@@ -32,9 +32,9 @@ const navBar = {
 				<div class="btn btn-nav-control fz0" @click="isNavOpen = !isNavOpen" :id="'btn-nav' + isMBSuffix">開關</div>
 				<nav class="nav">
 					<ul>
-						<li class="btn btn-nav"><a href="./Index" :id="'btn-nav-index' + isMBSuffix">事前預約</a></li>
-						<li class="btn btn-nav"><a href="./Class" :id="'btn-nav-class' + isMBSuffix">職業改編</a></li>
-						<li class="btn btn-nav"><a href="./More" :id="'btn-nav-more' + isMBSuffix">更多活動</a></li>
+						<li class="btn btn-nav"><a href="./class.html" :id="'btn-nav-index' + isMBSuffix">事前預約</a></li>
+						<li class="btn btn-nav"><a href="./class2.html" :id="'btn-nav-class' + isMBSuffix">職業改編</a></li>
+						<li class="btn btn-nav"><a href="./class3.html" :id="'btn-nav-more' + isMBSuffix">更多活動</a></li>
 					</ul>
 				</nav>
 			</div>

c2919a1ad4a2a66447c62dccf21e67906b534779
psycho909 <psychosocial909@gmail.com>
2024-12-24 22:52:09 +0800

    sadasdsa

--- event5/js/lightbox.js
+++ event5/js/lightbox.js
@@ -1,30 +1,15 @@
-export function MessageLB(msg, url, callback) {
-	$.gbox.open(msg, {
-		addClass: "default",
-		hasCloseBtn: true,
-		hasActionBtn: false
-	});
-}
-
-export function LBImage(image) {
-	let HTML = `<div class="lb-image__box"><img src="${image}" alt="" /></div>`;
-	$.gbox.open(HTML, {
-		addClass: "default lb-image ",
-		hasCloseBtn: true,
-		hasActionBtn: false
-	});
-}
-
 export function LBVideo(data) {
 	let HTMLContent;
 
 	if (data.yt) {
-		// 處理 YouTube 影片
 		HTMLContent = `<div class="lb-video__box">
-            <iframe src="https://www.youtube.com/embed/${data.yt}?enablejsapi=1&amp;origin=https%3A%2F%2Falpha-tw.beanfun.com" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+            <iframe src="https://www.youtube.com/embed/${data.yt}?enablejsapi=1&amp;origin=https%3A%2F%2Falpha-tw.beanfun.com" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen="">
+            </iframe>
         </div>`;
 	} else if (data.fb) {
-		// 處理 Facebook 影片
 		HTMLContent = `
             <div id="fb-root"></div>
             <div class="lb-video__box">
@@ -37,49 +22,59 @@ export function LBVideo(data) {
             </div>`;
 	}
 
+	// 載入 Facebook SDK 的函數
+	const loadFacebookSDK = () => {
+		return new Promise((resolve, reject) => {
+			if (window.FB) {
+				resolve();
+				return;
+			}
+
+			// 設定 callback
+			window.fbAsyncInit = function () {
+				FB.init({
+					appId: "YOUR_APP_ID",
+					xfbml: true,
+					version: "v17.0"
+				});
+				resolve();
+			};
+
+			// 載入 SDK
+			const js = document.createElement("script");
+			js.id = "facebook-jssdk";
+			js.src = "https://connect.facebook.net/en_US/sdk.js";
+			js.onerror = reject;
+
+			const fjs = document.getElementsByTagName("script")[0];
+			fjs.parentNode.insertBefore(js, fjs);
+		});
+	};
+
+	// 初始化 Facebook 影片
+	const initFacebookVideo = async () => {
+		try {
+			await loadFacebookSDK();
+			// 確保元素已經在 DOM 中
+			setTimeout(() => {
+				if (window.FB) {
+					FB.XFBML.parse();
+				}
+			}, 100);
+		} catch (error) {
+			console.error("Failed to initialize Facebook video:", error);
+		}
+	};
+
 	// 開啟彈窗
 	$.gbox.open(HTMLContent, {
 		addClass: "default lb-video",
 		hasCloseBtn: true,
-		hasActionBtn: false
-	});
-
-	// 確保 Facebook SDK 已加載
-	if (data.fb) {
-		if (!window.FB) {
-			(function (d, s, id) {
-				var js,
-					fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s);
-				js.id = id;
-				js.src = "https://connect.facebook.net/en_US/sdk.js";
-				fjs.parentNode.insertBefore(js, fjs);
-			})(document, "script", "facebook-jssdk");
+		hasActionBtn: false,
+		afterOpen: function () {
+			if (data.fb) {
+				initFacebookVideo();
+			}
 		}
-
-		// 初始化 Facebook SDK 並解析 fb-video
-		window.fbAsyncInit = function () {
-			FB.init({
-				appId: "YOUR_APP_ID", // 如果沒有應用程式 ID，可以留空
-				xfbml: true,
-				version: "v17.0"
-			});
-			FB.XFBML.parse(); // 解析 fb-video 標籤
-		};
-	}
-}
-
-export function LBRole(data) {
-	let HTML = `<div class="lb-role__box">
-		<div class="lb-role__body" data-role="${data.role}">
-			<div class="lb-role__title">${data.title}</div>
-			<div class="lb-role__text">${data.text}</div>
-		</div>
-	</div>`;
-	$.gbox.open(HTML, {
-		addClass: "default lb-role",
-		hasCloseBtn: true,
-		hasActionBtn: false
 	});
 }

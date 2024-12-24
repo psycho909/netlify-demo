ec3a8ab26b97065c472347d9cfdcdfa936ef5c0f
psycho909 <psychosocial909@gmail.com>
2024-12-24 22:43:18 +0800

    asdasd

--- event5/js/lightbox.js
+++ event5/js/lightbox.js
@@ -19,24 +19,55 @@ export function LBVideo(data) {
 	let HTMLContent;
 
 	if (data.yt) {
+		// 處理 YouTube 影片
 		HTMLContent = `<div class="lb-video__box">
-			<iframe src="https://www.youtube.com/embed/${data.yt}?enablejsapi=1&amp;origin=https%3A%2F%2Falpha-tw.beanfun.com" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
-		</div>`;
+            <iframe src="https://www.youtube.com/embed/${data.yt}?enablejsapi=1&amp;origin=https%3A%2F%2Falpha-tw.beanfun.com" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        </div>`;
 	} else if (data.fb) {
-		HTMLContent = `<div class="lb-video__box">
-			<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=${data.fb}&show_text=false&width=560&t=0"        width="560" 
-        height="314" 
-        style="border:none;overflow:hidden" 
-        scrolling="no" 
-        frameborder="0" 
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-		</div>`;
+		// 處理 Facebook 影片
+		HTMLContent = `
+            <div id="fb-root"></div>
+            <div class="lb-video__box">
+                <div class="fb-video" 
+                     data-href="${data.fb}" 
+                     data-width="560" 
+                     data-height="314" 
+                     data-allowfullscreen="false">
+                </div>
+            </div>`;
 	}
+
+	// 開啟彈窗
 	$.gbox.open(HTMLContent, {
 		addClass: "default lb-video",
 		hasCloseBtn: true,
 		hasActionBtn: false
 	});
+
+	// 確保 Facebook SDK 已加載
+	if (data.fb) {
+		if (!window.FB) {
+			(function (d, s, id) {
+				var js,
+					fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = "https://connect.facebook.net/en_US/sdk.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			})(document, "script", "facebook-jssdk");
+		}
+
+		// 初始化 Facebook SDK 並解析 fb-video
+		window.fbAsyncInit = function () {
+			FB.init({
+				appId: "YOUR_APP_ID", // 如果沒有應用程式 ID，可以留空
+				xfbml: true,
+				version: "v17.0"
+			});
+			FB.XFBML.parse(); // 解析 fb-video 標籤
+		};
+	}
 }
 
 export function LBRole(data) {
